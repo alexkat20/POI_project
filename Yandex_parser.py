097b4f2defad4da09f2e2534253f6bb816264b68
alexkat20 <alexkat20@rambler.ru>
2023-10-21 20:51:35 +0300

    Refactored parser and added wait_for_presence method

--- Yandex_parser.py
+++ Yandex_parser.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.common.exceptions import NoSuchElementException
@@ -16,154 +18,163 @@ class GrabberApp:
 
     def __init__(self, city):
         self.city = city
-        self.df = pd.DataFrame({"review": [], "date": []})
+        self.df = pd.DataFrame({"place": [], "review": [], "date": []})
 
-    def grab_data(self):
-        # chrome_options = webdriver.ChromeOptions
         # chrome_options.add_argument("--no-sandbox")
         # chrome_options.add_argument("--window-size=1420,1080")
-        # chrome_options.add_argument("--headless")
+
         # chrome_options.add_argument("--disable-gpu")
 
         options = webdriver.ChromeOptions()
+        #  options.add_argument("--headless")
         options.headless = False
         options.page_load_strategy = "none"
 
         chrome_path = ChromeDriverManager().install()
         chrome_service = Service(chrome_path)
+        self.driver = Chrome(options=options, service=chrome_service)
 
-        driver = Chrome(options=options, service=chrome_service)
+    def wait_for_presence(self, item):
+        for i in range(10):
+            try:
+                WebDriverWait(self.driver, 3).until(
+                    EC.presence_of_element_located(
+                        (By.XPATH, item)
+                    )
+                )
+            except:
+                continue
 
-        driver.maximize_window()
-        driver.get('https://yandex.ru/maps')
-        sleep(2)
+
+    def grab_data(self):
+        #  self.driver.maximize_window()
+        self.driver.get('https://yandex.ru/maps')
+
+        self.wait_for_presence('(//input[contains(@class,"input__control _bold")])')
 
         # Вводим данные поиска
-        driver.find_element(
-            By.XPATH, f'(//input[contains(@class,"input__control _bold")])'
+        self.driver.find_element(
+            By.XPATH, '(//input[contains(@class,"input__control _bold")])'
         ).send_keys(self.city)
 
+        self.wait_for_presence('(//div[contains(@class,"small-search-form-view__icon _type_search")])')
         # Нажимаем на кнопку поиска
-        # driver.find_element(By.CLASS_NAME, 'small-search-form-view__button').click()
-        driver.find_element(
+        self.driver.find_element(
             By.XPATH, f'(//div[contains(@class,"small-search-form-view__icon _type_search")])'
         ).click()
-        sleep(2)
-        driver.find_element(
-            By.XPATH, f'(//div[contains(@class,"tabs-select-view__title _name_inside")])'
-        ).click()
-        sleep(2)
+        self.wait_for_presence('(//div[contains(@class,"tabs-select-view__title _name_inside")])')
 
-        driver.find_element(
-            By.XPATH, f'(//div[contains(@class,"rating-ranking-view")])'
-        ).click()
-        sleep(5)
+        try:
+            self.driver.find_element(
+                By.XPATH, f'(//div[contains(@class,"tabs-select-view__title _name_inside")])'
+            ).click()
+        except:
+            self.driver.quit()
 
-        parent_handle = driver.window_handles[0]
-        i = 6
+        parent_handle = self.driver.window_handles[0]
+        i = 2
 
         while i:
-            sleep(1)
             url = None
 
             try:
-                WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located(
-                        (By.XPATH, f'(//div[contains(@class,"search-business-snippet-view__content")])[{i}]')
-                    )
-                )
-                org_info = driver.find_element(
+                self.wait_for_presence(f'(//div[contains(@class,"search-business-snippet-view__content")])[{i}]')
+                org_info = self.driver.find_element(
                     By.XPATH, f'(//div[contains(@class,"search-business-snippet-view__content")])[{i}]'
                 )
-
                 org_name = org_info.text.split("\n")[:3]
                 if org_name[0].isdigit():
-                    org_name = org_name[1:]
+                    org_name = org_name[1:2]
                 else:
                     org_name = org_name[:2]
-                print(*org_name)
+                if len(org_name) == 0:
+                    i += 1
+                    continue
+                print(org_name)
                 elements = org_info.find_elements(By.TAG_NAME, "a")
                 for el in elements:
                     link = el.get_attribute("href")
                     if "review" in link:
                         url = link
-
+                print(url)
                 org_info.location_once_scrolled_into_view
                 if not url:
                     i += 1
                     continue
-                driver.execute_script(f'window.open("{url}","org_tab");')
+                self.driver.execute_script(f'window.open("{url}","org_tab");')
 
-                child_handle = [x for x in driver.window_handles if x != parent_handle][0]
-                driver.switch_to.window(child_handle)
+                child_handle = [x for x in self.driver.window_handles if x != parent_handle][0]
+                self.driver.switch_to.window(child_handle)
 
-                sleep(2)
                 try:
-                    WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located(
-                            (By.XPATH, f'(//div[contains(@class,"tabs-select-view__title _name_reviews _selected")])')
-                        )
-                    )
 
-                    review_number = driver.find_element(
+                    self.wait_for_presence(
+                        '(//div[contains(@class,"rating-ranking-view")])')
+                    self.driver.find_element(
+                        By.XPATH, f'(//div[contains(@class,"rating-ranking-view")])'
+                    ).click()
+                    self.driver.find_element(
+                        By.XPATH, f'(//div[contains(@class,"rating-ranking-view__popup-line")])[2]'
+                    ).click()
+
+                    self.wait_for_presence('(//div[contains(@class,"tabs-select-view__title _name_reviews _selected")])')
+
+                    review_number = self.driver.find_element(
                         By.XPATH, f'(//div[contains(@class,"tabs-select-view__title _name_reviews _selected")])'
                     ).get_attribute("aria-label").split(", ")[1]
 
-                    print(review_number)
-
                     j = 0
                 except:
-                    driver.close()
-                    driver.switch_to.window(parent_handle)
-                    sleep(1)
+                    self.driver.close()
+                    self.driver.switch_to.window(parent_handle)
 
                     i += 1
                     continue
+
                 while j != int(review_number):
                     if j % 20 == 0:
                         sleep(2)
-                    WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located(
-                            (By.XPATH, f'(//span[contains(@class,"business-review-view__body-text")])[{j + 1}]')
-                        )
-                    )
 
-                    review = driver.find_element(
+                    self.wait_for_presence(f'(//span[contains(@class,"business-review-view__body-text")])[{j + 1}]')
+
+                    review = self.driver.find_element(
                         By.XPATH, f'(//span[contains(@class,"business-review-view__body-text")])[{j + 1}]'
                     )
 
                     review.location_once_scrolled_into_view
 
-                    date = driver.find_element(
+                    date = self.driver.find_element(
                         By.XPATH, f'(//span[contains(@class,"business-review-view__date")])[{j + 1}]'
                     )
 
-                    print(review.text, date.text)
-                    self.df.loc[len(self.df)] = {"review": review.text, "date": date.text}
+                    #  print(review.text, date.text)
+                    self.df.loc[len(self.df)] = {"place": " ".join(org_name), "review": review.text, "date": date.text}
 
                     if len(date.text.split()) > 2:
+                        i += 1
                         break
 
                     j += 1
+                print("--------------------------------------------------------------------")
 
-                driver.close()
-                driver.switch_to.window(parent_handle)
-                sleep(1)
+                self.driver.close()
+                self.driver.switch_to.window(parent_handle)
 
                 i += 1
 
             except:
                 break
 
-
-
-        driver.quit()
+        self.driver.quit()
+        return self.df
 
 
 def main():
     city = input('Область поиска: ')
     grabber = GrabberApp(city)
-    grabber.grab_data()
+    data = grabber.grab_data()
+    data = data.dropna(how="all", subset=["place"], ignore_index=True)
+    data.to_csv("data.csv")
 
 
 if __name__ == '__main__':
